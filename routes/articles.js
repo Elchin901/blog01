986c8d5f4c77c1db77c45953dd59ec76c6a41414
Elchin901 <73276152+Elchin901@users.noreply.github.com>
2023-08-09 14:09:12 +0200

    first commit

---
+++ routes/articles.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/new',(req, res)=>{
+    res.render('articles/new.ejs')
+})
+
+router.post('/',(req,res) =>{
+    res.render('articles/new.ejs')
+})
+module.exports = router
